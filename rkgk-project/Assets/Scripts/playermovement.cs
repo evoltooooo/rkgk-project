869f95575324a62895f0ce593c2176f15ff4ffd5
evoltooooo <infernodragon410@gmail.com>
2026-03-11 17:55:12 +0700

    added playermovement script

---
+++ rkgk-project/Assets/Scripts/playermovement.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class player : MonoBehaviour
+{
+    [Header("Player Component References")]
+    [SerializeField] Rigidbody2D rb;
+
+    [Header("Player Settigns")]
+    [SerializeField] float speed;
+    [SerializeField] float jumpingPower;
+
+    [Header("Grounding")]
+    [SerializeField] LayerMask groundLayer;
+    [SerializeField] Transform groundCheck;
+
+    private float horizontal;
+
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(horizontal * speed, rb.linearVelocity.y);
+    }
+
+    #region PLAYER_CONTROLS
+    public void Move(InputAction.CallbackContext context)
+    {
+        horizontal = context.ReadValue<Vector2>().x;
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if(context.performed && IsGrounded())
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpingPower);
+        } 
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCapsule(groundCheck.position, new Vector2(1f, 0.1f), CapsuleDirection2D.Horizontal, 0, groundLayer);
+    }
+    #endregion
+}
